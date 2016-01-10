628bc201a6c139399181d9a4f65cc74afdd5a4ad
Jelle Licht <wordempire@gmail.com>
2016-01-10 16:49:20 +0100

    Update sdp.m

--- assignment2/sdp.m
+++ assignment2/sdp.m
@@ -1,42 +1,39 @@
+% sdp
+%   Calculate the lower bound, upper bound and running time for different GW randomized rounds
+% Arguments
+%   W - The adjacency matrix of the MAXCUT instance
 function lines = sdp(W);
+
 
 tic
 n = length(W);
 Y = sdpvar(n,n);
 lines = [];
-% tempMatrix = zeros(n,n);
-% theSum = 0;
-% for col = 1:n
-%     for row = 1:n
-%         if row >= col
-%             break;
-%         end
-%         theSum = theSum +(W(row,col)*(1-Y(row,col))); 
-%     end
-% end
-% h = -1/2*theSum;
 
+% see report for deriviation of this objective function
 h = -1/4 * sum(dot(W,1-Y));
 c = [diag(Y) == ones(n,1), Y >=0];
 ops = sdpsettings('solver','sedumi','verbose',0);
 sol = solvesdp(c, h, ops);
 UB = double(-h);
-% sol.solvertime
 
 y = double(Y);
 [Q,A] = eig(y);
 B = Q*sqrt(A);
-%if isequal(y,B*B')
-%fprintf('cholesky correct');
-%end
+
+% By this point, y should equal B*B'
+
+% The time needed to calculate the SDP matrix B
 preTime = toc
 
 Ts = [1 5 10 25 50 75 100];
 for t = Ts
     tic
     LB = GW(B,W,t);
+    % Th time needed for the randomized GW rounds
     postTime = toc
     elapsed = preTime + postTime;
     pre = [LB, UB, elapsed];
+    % Write each set of esults to the return value
     lines = [lines; pre];     
 end
